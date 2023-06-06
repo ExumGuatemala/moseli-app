0c340b5a64e9f190d7ed67582f6a73ae8917d0ab
Kenny Barrera <barrerakj@gmail.com>
2023-06-06 11:43:46 -0600

    Cambios en las migraciones

--- database/migrations/2023_06_02_164159_add_key_to_order.php
+++ database/migrations/2023_06_02_164159_add_key_to_order.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->string('key');
+            $table->string('key')->after('description')->nullable();
         });
     }
 
