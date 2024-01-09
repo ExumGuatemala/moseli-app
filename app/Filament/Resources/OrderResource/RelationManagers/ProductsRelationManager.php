cb5b5e33eef62a4cf44ef89f29e4221b4c912ef5
Kenny Barrera <barrerakj@gmail.com>
2024-01-08 18:13:40 -0600

    Agregar view action y action group

--- app/Filament/Resources/OrderResource/RelationManagers/ProductsRelationManager.php
+++ app/Filament/Resources/OrderResource/RelationManagers/ProductsRelationManager.php
@@ -20,6 +20,8 @@ use Filament\Tables\Actions\EditAction;
 use Filament\Tables\Actions\Action;
 use Filament\Tables;
 use App\Services\OrderService;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Actions\ViewAction;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -219,87 +221,93 @@ class ProductsRelationManager extends RelationManager
                 Tables\Actions\CreateAction::make(),
             ])
             ->actions([
-                Action::make("goToProduct")
-                    ->label("Ver Producto")
-                    ->action(function (Model $record) {
-                        redirect()->intended('/admin/products/'.str($record->id));
-                    }),
-                EditAction::make()
-                    ->label('Editar Producto')
-                    ->slideOver()
-                    ->modalWidth('4xl')
-                    ->modalHeading('Editar Producto')
-                    ->modalButton('Guardar')
-                    ->form(fn (EditAction $action): array => [
-                        TextInput::make('productName')
-                            ->label('Producto')
-                            ->disabled()
-                            ->afterStateHydrated(function (TextInput $component) use ($action) {
-                                $component->state($action->getRecordTitle());
-                            }),
-                        TextInput::make('quantity')
-                            ->label('Cantidad a comprar')
-                            ->required()
-                            ->default(1),
-                        Select::make('size')
-                            ->label('Talla')
-                            ->afterStateHydrated(function (Model|null $record, Select $component) {
-                                $record == null ? $component->state(null) : $component->state($record->size);
-                            })
-                            ->options([
-                                '2' => '2',
-                                '4' => '4',
-                                '6' => '6',
-                                '8' => '8',
-                                '10' => '10',
-                                '12' => '12',
-                                '14' => '14',
-                                'XS' => 'XS',
-                                'S' => 'S',
-                                'M' => 'M',
-                                'L' => 'L',
-                                'XL' => 'XL',
-                            ]),
-                        Select::make('colors')
-                            ->multiple()
-                            ->label('Color')
-                            ->options(ProductColor::all()->pluck('name', 'id')),
-                        Toggle::make('has_embroidery')->inline()
-                            ->label('Agregar bordado?')
-                            ->reactive(),
-                        TextInput::make('embroidery')
-                            ->label('Texto de Bordado')
-                            ->hidden(
-                                fn (Closure $get): bool => $get('has_embroidery') == false
-                            ),
-                        Toggle::make('has_sublimate')->inline()
-                            ->label('Agregar sublimado?')
-                            ->reactive(),
-                        TextInput::make('sublimate')
-                            ->label('Texto de sublimado')
-                            ->hidden(
-                                fn (Closure $get): bool => $get('has_sublimate') == false
-                            ),
-                    ])
-                    ->mutateFormDataUsing(function (array $data): array {
-                        $data['colors'] = json_encode($data['colors']);
-                        return $data;
-                    })
-                    ->after(function (RelationManager $livewire) {
-                        self::$orderService->updateTotal($livewire->ownerRecord->id);
-                        self::$orderService->updateBalance($livewire->ownerRecord->id); 
-                        $livewire->emit('refresh');
-                    }),
-                DetachAction::make()
-                    ->label('Quitar')
-                    ->modalHeading('Quitar de la orden')
-                    ->modalSubheading('Esta accion es permanente, desea continuar con la eliminación?')
-                    ->modalButton('Si, deseo quitarlo')
-                    ->after(function (RelationManager $livewire) {
-                        self::$orderService->updateTotal($livewire->ownerRecord->id);
-                        self::$orderService->updateBalance($livewire->ownerRecord->id);                        
-                        $livewire->emit('refresh');
-                    }),
+                ActionGroup::make([
+                    Action::make("goToProduct")
+                        ->icon('heroicon-o-document')
+                        ->label("Ver Producto Original")
+                        ->action(function (Model $record) {
+                            redirect()->intended('/admin/products/'.str($record->id));
+                        }),
+                    ViewAction::make()
+                        ->label("Ver Producto")
+                        ->slideOver(),
+                    EditAction::make()
+                        ->label('Editar Producto')
+                        ->slideOver()
+                        ->modalWidth('4xl')
+                        ->modalHeading('Editar Producto')
+                        ->modalButton('Guardar')
+                        ->form(fn (EditAction $action): array => [
+                            TextInput::make('productName')
+                                ->label('Producto')
+                                ->disabled()
+                                ->afterStateHydrated(function (TextInput $component) use ($action) {
+                                    $component->state($action->getRecordTitle());
+                                }),
+                            TextInput::make('quantity')
+                                ->label('Cantidad a comprar')
+                                ->required()
+                                ->default(1),
+                            Select::make('size')
+                                ->label('Talla')
+                                ->afterStateHydrated(function (Model|null $record, Select $component) {
+                                    $record == null ? $component->state(null) : $component->state($record->size);
+                                })
+                                ->options([
+                                    '2' => '2',
+                                    '4' => '4',
+                                    '6' => '6',
+                                    '8' => '8',
+                                    '10' => '10',
+                                    '12' => '12',
+                                    '14' => '14',
+                                    'XS' => 'XS',
+                                    'S' => 'S',
+                                    'M' => 'M',
+                                    'L' => 'L',
+                                    'XL' => 'XL',
+                                ]),
+                            Select::make('colors')
+                                ->multiple()
+                                ->label('Color')
+                                ->options(ProductColor::all()->pluck('name', 'id')),
+                            Toggle::make('has_embroidery')->inline()
+                                ->label('Agregar bordado?')
+                                ->reactive(),
+                            TextInput::make('embroidery')
+                                ->label('Texto de Bordado')
+                                ->hidden(
+                                    fn (Closure $get): bool => $get('has_embroidery') == false
+                                ),
+                            Toggle::make('has_sublimate')->inline()
+                                ->label('Agregar sublimado?')
+                                ->reactive(),
+                            TextInput::make('sublimate')
+                                ->label('Texto de sublimado')
+                                ->hidden(
+                                    fn (Closure $get): bool => $get('has_sublimate') == false
+                                ),
+                        ])
+                        ->mutateFormDataUsing(function (array $data): array {
+                            $data['colors'] = json_encode($data['colors']);
+                            return $data;
+                        })
+                        ->after(function (RelationManager $livewire) {
+                            self::$orderService->updateTotal($livewire->ownerRecord->id);
+                            self::$orderService->updateBalance($livewire->ownerRecord->id); 
+                            $livewire->emit('refresh');
+                        }),
+                    DetachAction::make()
+                        ->label('Quitar')
+                        ->modalHeading('Quitar de la orden')
+                        ->modalSubheading('Esta accion es permanente, desea continuar con la eliminación?')
+                        ->modalButton('Si, deseo quitarlo')
+                        ->after(function (RelationManager $livewire) {
+                            self::$orderService->updateTotal($livewire->ownerRecord->id);
+                            self::$orderService->updateBalance($livewire->ownerRecord->id);                        
+                            $livewire->emit('refresh');
+                        }),
+                ]),
             ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
