7173c15389608312905bb0793be33dc7e58a1ada
Kenny Barrera <barrerakj@gmail.com>
2025-12-16 08:39:45 -0600

    MOS-69 | crear migracion para campos de talla especial

---
+++ database/migrations/2025_12_06_144519_tbl_orders_products_add_rows_special_size.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders_products', function (Blueprint $table) {
+            $table->string('has_special_size')->nullable()->after('size');
+            $table->string('special_size')->nullable()->after('size');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order_products', function (Blueprint $table) {
+            $table->dropColumn('has_special_size');
+            $table->dropColumn('special_size');
+        });
+    }
+};
